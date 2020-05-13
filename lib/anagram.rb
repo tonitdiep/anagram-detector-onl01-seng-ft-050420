bf01af13e85b41fa0a9de15305149987403fec85
Toni  Diep <adiep123@gmail.com>
2020-05-13 01:41:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+attr_accessor :words 
+  
+  def initialize(words)
+    @words = words
+  end
+
+  def match(some_words)
+  some_words.select do |some_words|
+  @words.split("").sort == some_words.split("").sort
+  end
+  end
+  # .sort .split() .detect ==
+  #return all matches in an array or otherwise, return nil/empty array
+
+# binding.pry
+end
